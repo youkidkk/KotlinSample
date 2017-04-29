87588aa1e0aa3130d928fe06b9df2ffe95a64f29
youkidkk <youkidkk80@gmail.com>
2017-04-29 17:09:18 +0900

    ConstructorSample を追加

---
+++ src/main/kotlin/sample/classes/ConstructorSample.kt
@@ -0,0 +1,30 @@
+package sample.classes
+
+class ConstructorSample(val number : Int,  // valを指定すると、プロパティとなる
+             val text : String,
+             arg : String       // valを指定しなかった場合はプロパティとならず、フィールドの初期化に使用できる
+    ) {
+
+    // セカンダリーコンストラクタ
+    // this を使用してプライマリーコンストラクタを呼び出す
+    constructor(number : Int, text : String) : this(number, text, "")
+
+    // コンストラクタの引数を使用して初期化が可能
+    val length = arg.length
+
+    fun print() {
+        println("ConstructorSample : ")
+        println("  Number : ${number}")
+        println("  Text : ${text}")
+        println("  Length : ${length}")
+    }
+
+}
+
+fun main(args: Array<String>) {
+    // プライマリーコンストラクタを使用
+    ConstructorSample(1, "Test", "Test Test Test").print()
+
+    // セカンダリーコンストラクタを使用
+    ConstructorSample(2, "Test Test").print()
+}
