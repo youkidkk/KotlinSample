1f8d9c09d54bb25886d18eba6e0baff64da1dddb
youkidkk <youkidkk80@gmail.com>
2017-04-29 15:25:22 +0900

    WhenSample に引数なしのサンプルを追加

--- src/main/kotlin/controlflow/sample/WhenSample.kt
+++ src/main/kotlin/controlflow/sample/WhenSample.kt
@@ -11,10 +11,18 @@ fun main(args: Array<String>) {
     }
 
     val s2 : String = when(s) {
-        "hoge" -> "hoge hoge"
-        "fuga" -> "fuga fuga"
+        "hoge" -> "hoge hoge."
+        "fuga" -> "fuga fuga."
         // if と同様に else句が必須となる
-        else -> "other"
+        else -> "other."
     }
     println(s2)
+
+    // 引数を与えずに分岐する記法
+    val s3 : String = when {
+        s == "hoge" -> "hoge hoge hoge."
+        s == "fuga" -> "fuga fuga fuga."
+        else -> "other."
+    }
+    println(s3)
 }
