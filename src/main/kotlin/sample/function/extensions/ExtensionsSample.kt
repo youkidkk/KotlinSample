10dac58bc8dd31196aff61581b04ec4076030ba9
youkidkk <youkidkk80@gmail.com>
2017-05-13 22:34:02 +0900

    ExtensionSample を追加

---
+++ src/main/kotlin/sample/function/extensions/ExtensionsSample.kt
@@ -0,0 +1,18 @@
+package sample.function.extensions
+
+/**
+ * 拡張関数のサンプル。
+ * 既存の型にメソッドを追加することができる。
+ */
+
+/** 【型名】.【関数名】で宣言する。 */
+fun String.println() {
+    println(this + "!!!")
+}
+
+fun main(args: Array<String>) {
+    val string = "hoge hoge piyo piyo"
+
+    /** String型にprintlnが追加されている */
+    string.println()
+}
