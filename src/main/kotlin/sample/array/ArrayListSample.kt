194caf7ae7540f6da67d716631a113701215f185
youkidkk <youkidkk80@gmail.com>
2017-05-21 18:44:34 +0900

    ArrayListSample を更新

--- src/main/kotlin/sample/array/ArrayListSample.kt
+++ src/main/kotlin/sample/array/ArrayListSample.kt
@@ -2,8 +2,9 @@ package sample.array
 
 fun main(args: Array<String>) {
     // List の生成
-    // Java標準の List のインスタンスを生成する
+    // Java標準の ArrayList のインスタンスを生成する
     val list = arrayListOf("hoge", "fuga", "piyo")
+    println(list.javaClass) // -> class java.util.ArrayList
 
     for (item in list) {
         println(item)
