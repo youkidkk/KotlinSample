fd185038602b770a06b82eda3bf15d53ef1717c3
youkidkk <youkidkk80@gmail.com>
2017-05-24 21:47:35 +0900

    高階関数のサンプルを更新

--- src/main/kotlin/sample/function/HigherOrderFunctionSample.kt
+++ src/main/kotlin/sample/function/HigherOrderFunctionSample.kt
@@ -4,18 +4,20 @@ package sample.function
  * 高階関数サンプル。
  */
 
-// 引数に関数オブジェクトをとる関数 -> 高階関数
+// 高階関数 : 引数に関数オブジェクトをとる関数
 fun func(s: String, funcArg: (String) -> Int) : String {
     return "arg : $s, func result : ${funcArg(s)}"
 }
 
 fun main(args: Array<String>) {
-    // 引数の変数名を指定する記法
-    println(func("hoge",{ s -> s.length }))
+    // 引数の変数名、型を指定する記法
+    println(func("hoge", { s: String -> s.length }))
+    // 引数の変数名のみ指定し、型推論により型を省略した記法
+    println(func("hogehoge", { s -> s.length }))
     // 引数が一つの場合に、変数名を省略して「it」で引数を指定する記法
-    println(func("hogehoge", { it.length }))
+    println(func("hogehogehoge", { it.length }))
     // 引数の最後がラムダ式の場合に、そのラムダ式を引数リストの外に置く記法
-    println(func("hogehogehoge") { it.length })
+    println(func("hogehogehogehoge") { it.length })
     // メソッド参照
     println(func("fuga", ::argFunc))
 }
